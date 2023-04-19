3c6f3058c4b47c3282cb57118ce79edc2f01e8b0
Ricardo Ruiz <riru.ruiz18@gmail.com>
2023-04-19 13:08:40 -0600

    update

--- 01-Fundamentals-Part-1/starter/main.js
+++ 01-Fundamentals-Part-1/starter/main.js
@@ -1,5 +1,29 @@
-let js = "amazing";
+/* let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
 console.log("Jonas");
 console.log(23);
+
+let firstName = "Jonas";
+console.log(firstName);*/
+
+const now = 2023;
+const ageRick = now - 2000;
+const ageOri = now - 1992;
+console.log(ageRick, ageOri);
+
+console.log(ageRick * 2, ageRick / 10, 2 ** 3);
+
+const firstName = 'Rick';
+const lastName = 'Ruiz';
+console.log(firstName + ' ' + lastName);
+
+let x = 10 + 5;
+x += 10;
+x++;
+x--;
+x--;
+console.log(x);
+
+
+console.log(ageRick > ageOri);
